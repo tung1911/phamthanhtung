f68ea3b86f5e4816877c2046df226b3b07a2f323
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-17 20:00:53 +0700

    Add files via upload

---
+++ chuyen doi du lieu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	system("cls");
+	
+	printf("40/17*13/3=%d\n",40/17*13/3);
+	printf("40/17*13/3.0=%f\n",40/17*13/3.0);
+	printf("40/17*13.0/3=%f\n",40/17*13.0/3);
+	printf("40/17.0*13/3=%f\n",40/17.0*13/3);
+	return 0;
+}
