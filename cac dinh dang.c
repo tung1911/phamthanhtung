7f759c23c17cc6f89e21c423831cfdf54903dfe7
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-17 20:36:52 +0700

    Add files via upload

---
+++ cac dinh dang.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a= 10;
+    float b = 24.67892345;
+    char ch = 'A';
+    
+    printf("integer dât = %d\n", a);
+    printf("float data =%f\n", b);
+    printf("character = %c\n", ch);
+    printf("This prints the string\n");
+    printf("%s\n","This also prints a string");
+	return 0;
+}
