683fdf23ad53c973fba3c43c6e9828666af10374
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-17 20:40:59 +0700

    Add files via upload

---
+++ modifier.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	 printf("The number 555 in various form: \n");
+    printf("Without any modifier: \n");
+    printf("[%d]\n", 555);
+    
+    printf("With - modifier : \n");
+    printf("[%-d]\n", 555);
+    
+    printf("With digit string 10 as modifier :\n");
+    printf("[%10d]\n", 555);
+    
+    printf("With 0 as modifer : \n");
+    printf("[%0d]\n", 555);
+    
+    printf("With 0 and digit string 10 as modifers :\n");
+    printf("[%010d]\n", 555);
+    
+    printf("With -, 0 and digit string 10 as modifiers :\n");
+    printf("[% - 010d]\n", 555);
+	return 0;
+}
