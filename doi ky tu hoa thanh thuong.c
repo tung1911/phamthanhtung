ea2cdeaaed17eec5ab81996999b1b8f53c681b88
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-20 20:08:44 +0700

    Add files via upload

---
+++ doi ky tu hoa thanh thuong.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char c;
+	
+	printf("Please enter a character:");
+	scanf("%c",&c);
+	
+	if(c>="A" && c<='Z')
+	printf("Lowercase charater = %c",c + 'a'-'A');
+	
+	else
+	printf("Character Entered is=%c",c);
+	return 0;
+}
