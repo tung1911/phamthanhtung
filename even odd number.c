f007a6c03ce35b635c1dc2d9b74ead014bcd8c00
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-20 20:08:54 +0700

    Add files via upload

---
+++ even odd number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num,res;
+	
+	printf("enter a number:");
+	scanf("%d",&num);
+	
+	res=num%2;
+	if(res==0)
+	printf("the number is Even");
+	
+	else
+	printf("the number is Odd");
+	
+	return 0;
+}
