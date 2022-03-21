ba71cc53dc35f8b3497c0e556aa0168f72092877
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-21 09:53:27 +0700

    Add files via upload

---
+++ vong lap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int count;
+	printf("\t This is a \n");
+	
+	for(count=1; count <=100;count+=99){
+	printf("\n\t\t nice");
+	}
+	printf("\n\t\t world.\n");
+	return 0;
+}
